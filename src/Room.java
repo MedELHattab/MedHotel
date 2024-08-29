13998049fdc737e2004bf2bec2c06c56d1a75ea5
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2024-08-29 11:28:51 +0100

    I implemented The code of the Room class

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,31 @@
 public class Room {
+    private final int roomNumber;
+    private boolean isAvailable;
+
+
+    public Room(int roomNumber) {
+        this.roomNumber = roomNumber;
+        this.isAvailable = true;  // Par défaut, la chambre est disponible
+
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void bookRoom() {
+        if (isAvailable) {
+            this.isAvailable = false;
+        } else {
+            System.out.println("La chambre est déjà réservée.");
+        }
+    }
+
+    public void releaseRoom() {
+        this.isAvailable = true;
+    }
 }
