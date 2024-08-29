6381184994317b363534615a849a4da1157f1ab2
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2024-08-29 11:53:43 +0100

    I implemented the logic in Reservation class

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,2 +1,47 @@
+import java.util.Date;
+
 public class Reservation {
+    private final int reservationId;
+    private String guestName;
+    private Room room;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+    public Reservation(int reservationId, String guestName, Room room, Date checkInDate, Date checkOutDate) {
+        this.reservationId = reservationId;
+        this.guestName = guestName;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void modifyReservation(Date newCheckInDate, Date newCheckOutDate) {
+        this.checkInDate = newCheckInDate;
+        this.checkOutDate = newCheckOutDate;
+    }
+
+    public void cancelReservation() {
+        room.releaseRoom();
+    }
 }
+
